eeff679c7be964af22d5d403ca2d3ba8038ac855
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-08-24 20:27:16 -0300

    re-initial project

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangoproject project.
+WSGI config for ecommerce project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoproject.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce.settings")
 
 application = get_wsgi_application()
