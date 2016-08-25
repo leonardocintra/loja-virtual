6c94420f58e9fb84d199fa62dd34705e7aa5793a
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-08-24 22:32:17 -0300

    allowwed-hots = full

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -90,7 +90,7 @@ DATABASES['default'].update(dj_database_url.config())
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Password validation
