6e85eb179ed2dc3544d3cd8ee0efc1a9379805fc
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-08-24 21:56:21 -0300

    implementado as url

--- core/views.py
+++ core/views.py
@@ -4,11 +4,14 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
+
 def contact(request):
     return render(request, 'contact.html')
 
+
 def product_list(request):
     return render(request, 'product_list.html')
+
 
 def product(request):
     return render(request, 'product.html')
