39707575485e69f514d3d3e4b772ce883b5ee5b0
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-08-24 21:05:38 -0300

    criado o index.html

--- core/views.py
+++ core/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Ronaldo testinho')
+    texts = ["maria puera", "Neymar fez gol na copa"]
+    context = {
+        "title": "django | e-commerce",
+        "texts": texts
+    }
+
+    return render(request, 'index.html', context)
