5f46f6e50e406103e8bb5649ad95f7dcff4dc90e
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-08-24 21:36:41 -0300

    criado o base.html

--- core/views.py
+++ core/views.py
@@ -2,10 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    texts = ["maria puera", "Neymar fez gol na copa"]
-    context = {
-        "title": "django | e-commerce",
-        "texts": texts
-    }
-
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
