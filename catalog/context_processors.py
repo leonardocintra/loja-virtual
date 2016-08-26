5ed2e3b3747dab7719269c4336302ffd8fe570e7
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-08-26 19:34:54 -0300

    criado o category_processors p exibir categorias

---
+++ catalog/context_processors.py
@@ -0,0 +1,6 @@
+from .models import Category
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -67,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # my apps
+                'catalog.context_processors.categories',
             ],
         },
     },
