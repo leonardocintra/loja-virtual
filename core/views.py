7bf1a72758d0da5045069a54b39c0ce6f6982a4b
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-09-12 19:29:33 -0300

    criado template para registro

--- core/views.py
+++ core/views.py
@@ -2,14 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.conf import settings
-from django.views.generic import View, TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from django.views.generic import View, TemplateView, CreateView
 
 from .forms import ContactForm
+
+
+User = get_user_model
+
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-index = IndexView.as_view()
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'register.html'
+    model = User
 
 
 def contact(request):
@@ -25,3 +34,8 @@ def contact(request):
         'success': success
     }
     return render(request, 'contact.html', context)
+
+
+
+index = IndexView.as_view()
+register = RegisterView.as_view()
