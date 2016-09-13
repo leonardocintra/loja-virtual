527179a8851094bedc53affb7b8c629fb299bb15
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-09-13 20:52:19 -0300

    criado admin User

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ['username', 'name', 'email', 'is_active', 'is_staff', 'date_joined']
+
+
+admin.site.register(User, UserAdmin)
+
+
