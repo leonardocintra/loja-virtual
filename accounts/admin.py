61b60ee0c295d0197dca0ee0caaf9a45772f9ef8
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-09-13 21:16:40 -0300

    finalizado ajuste do admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,33 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
+from .forms import UserAdminCreateForm, UserAdminForm
 
 
 class UserAdmin(BaseUserAdmin):
+    add_form =  UserAdminCreateForm
+    add_fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'password1', 'password2')
+        }),
+    ) 
+    form = UserAdminForm
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'email')
+        }),
+        ('Informações Básicas', {
+            'fields': ('name', 'last_login')
+        }),
+        (
+            'Permissões', {
+                'fields': (
+                    'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'
+                )
+            }
+        ),
+        # ('Datas', {'fiedls': ('last_login',)}),
+    )
     list_display = ['username', 'name', 'email', 'is_active', 'is_staff', 'date_joined']
 
 
---
+++ accounts/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .models import User
+
+class UserAdminCreateForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class UserAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'name', 'is_active', 'is_staff']
