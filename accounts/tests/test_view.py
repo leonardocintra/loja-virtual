7011e3ecca00e0d555efe35667755c7d26ef0402
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-09-14 20:42:49 -0300

    test_register_error

--- accounts/tests/test_view.py
+++ accounts/tests/test_view.py
@@ -1,13 +1,28 @@
 from django.test import Client, TestCase
 from django.core.urlresolvers import reverse
 
+from accounts.models import User
+
 
 class RegisterViewTestCase(TestCase):
+
     def setUp(self):
         self.client = Client()
         self.register_url = reverse('accounts:register')
     
     def test_register_ok(self):
+        data = {
+            'username': 'leonardo', 
+            'password1': 'teste123', 
+            'password2': 'teste123',
+            'email': 'test@teste.com'
+        }
+        response = self.client.post(self.register_url, data)
+        index_url = reverse('index')
+        self.assertRedirects(response, index_url)
+        self.assertEqual(User.objects.count(), 1)
+    
+    def test_register_error(self):
         data = {'username': 'leonardo', 'password1': 'teste123', 'password2': 'teste123'}
         response = self.client.post(self.register_url, data)
         self.assertFormError(response, 'form', 'email', 'Este campo é obrigatório.')
