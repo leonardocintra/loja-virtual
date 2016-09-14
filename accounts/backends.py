250b767477c136e43ac70b6f8e05596131cd4ee0
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-09-14 20:21:32 -0300

    feito o backend authentication

--- accounts/backends.py
+++ accounts/backends.py
@@ -1,7 +1,14 @@
 from django.contrib.auth.backends import ModelBackend as BaseModelBackend
 
+from .models import User
 
 class ModelBackend(BaseModelBackend):
+    
     def authenticate(self, username=None, password=None):
         if username is None:
-            pass
+            try:
+                user = User.objects.get(email=username)
+                if user.check_password(password):
+                    return user
+            except User.DoesNotExist
+                pass
