a1b1d538c66d5c3d185bca87667187d8bed7438f
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-09-15 20:47:30 -0300

    implementado update de dados do usuario

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, UpdateView
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -18,5 +18,17 @@ class RegisterView(CreateView):
     success_url = reverse_lazy('index')
 
 
+class UpdateUserView(UpdateView):
+    model = User
+    template_name = 'accounts/update_user.html'
+    fields = ['name', 'email', ]
+    success_url = reverse_lazy('accounts:index')
+
+    def get_object(self):
+        return self.request.user
+
+
+
 index = IndexView.as_view()
 register = RegisterView.as_view()
+update_user = UpdateUserView.as_view()
