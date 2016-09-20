968f0b6e6e1c0c6a75f9192e94946a10ec82aed5
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-09-20 19:18:53 -0300

    created model checkout cart Iten

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class CartItem(models.Model):
+    cart_key = models.CharField('Chave do carrinho', max_length=40, db_index=True) 
+    product = models.ForeignKey('catalog.Product', verbose_name='Produto')
+    quantity = models.PositiveIntegerField('Quantidade', default=1)
+    price = models.DecimalField('Preço', decimal_places=2, max_digits=8)
+
+    class Meta:
+        verbose_name = 'Item do carrinho'
+        verbose_name_plural = 'Itens dos Carrinhos'
+    
+    def __str__(self):
+        return '{} [{}]'.format(self.product, self.quantity)
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'core',
     'accounts',
     'catalog',
+    'checkout',
 ]
 
 MIDDLEWARE = [
