396900a89f8966437f1c450145580d54757a26a5
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-09-21 19:45:06 -0300

    test post save cart item

--- checkout/tests/test_model.py
+++ checkout/tests/test_model.py
@@ -1,3 +1,18 @@
 from django.test import Client, TestCase
 from model_mommy import mommy
 
+from checkout.models import CartItem
+
+
+class CartItemTestCase(TestCase):
+    def setUp(self):
+         mommy.make(CartItem, _quantity=3)
+    
+    def tearDown(self):
+        pass
+    
+    def test_post_save_cart_item(self):
+        cart_item = CartItem.objects.all()[0]
+        cart_item.quantity = 0
+        cart_item.save()
+        self.assertEqual(CartItem.objects.count(), 2)
