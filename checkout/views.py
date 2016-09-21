205ed2aa949d48b69ca9797a5541dbd7de07539e
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-09-21 20:30:39 -0300

    create template checkout

--- checkout/views.py
+++ checkout/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import RedirectView, TemplateView
 from django.forms import modelformset_factory
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.urlresolvers import reverse_lazy
 
+
 from catalog.models import Product
-from .models import CartItem
+from .models import CartItem, Order
 
 
 
@@ -60,8 +62,25 @@ class CartItemView(TemplateView):
             messages.success(request, 'Carrinho atualizado com sucesso')
             context['formset'] = self.get_formset(clear=True)
         return self.render_to_response(context)
+
+
+class CheckoutView(LoginRequiredMixin, TemplateView):
+    template_name = 'checkout/checkout.html'
+
+    def get(self, request, *args, **kwargs):
+        session_key = request.session.session_key
+        if session_key and CartItem.objects.filter(cart_key=session_key).exists():
+            cart_items = CartItem.objects.filter(cart_key=session_key)
+            order = Order.objects.create_order(
+                user=request.user, cart_items=cart_items
+            )
+        else:
+            messages.info(request, 'Não há items no carrinho de compras')
+            return redirect('checkout:cart_item')
+        return super(CheckoutView, self).get(request, *args, **kwargs)
          
 
 
 create_cartitem = CreateCartItemView.as_view()
 cart_item = CartItemView.as_view()
+checkout = CheckoutView.as_view()
