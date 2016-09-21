4b7f9e72580c5fb0c76e7996e2edaea1bb972988
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-09-21 19:55:32 -0300

    test add mesmo item e quantidade = 2

--- checkout/tests/test_views.py
+++ checkout/tests/test_views.py
@@ -13,9 +13,16 @@ class CreateCartItemTestCase(TestCase):
     
     def tearDown(self):
         self.product.delete()
+        CartItem.objects.all().delete()
 
     def test_add_cart_item_simples(self):
         response = self.client.get(self.url)
         redirect_url = reverse('checkout:cart_item')
         self.assertEqual(response.status_code, 302)
         self.assertEqual(CartItem.objects.count(), 1)
+    
+    def test_add_mesmo_item_simples(self):
+        response = self.client.get(self.url)
+        response = self.client.get(self.url)
+        cart_item = CartItem.objects.get()
+        self.assertEqual(cart_item.quantity, 2)
