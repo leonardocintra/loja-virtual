2eedf93770e25873519ee58a560f59d93021c6f5
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-09-21 20:32:51 -0300

    add urls for checkout

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^carrinho/adicionar/(?P<slug>[\w_-]+)/$', views.create_cartitem, name='create_cartitem'),
     url(r'^carrinho/$', views.cart_item, name='cart_item'),
+    url(r'^finalizando/$', views.checkout, name='checkout')
 ]
