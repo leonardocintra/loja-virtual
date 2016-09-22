d5f308d4e1019b3a6778ad1fb57392091298434b
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-09-21 22:06:57 -0300

    update bug middleware

--- checkout/middleware.py
+++ checkout/middleware.py
@@ -7,7 +7,7 @@ def cart_item_middleware(get_response):
         
         response = get_response(request)
 
-        if session_key != request.session.session_key:
+        if session_key != request.session.session_key and request.session.session_key:
             CartItem.objects.filter(cart_key=session_key).update(cart_key=request.session.session_key)
         return response
     return middleware
