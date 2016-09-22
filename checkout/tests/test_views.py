372e8833c38977e3f395574f9cf679803de00ca5
Leonardo Cintra <leonardo.ncintra@outlook.com>
2016-09-21 21:59:36 -0300

    finalizado teste do Checkout

--- checkout/tests/test_views.py
+++ checkout/tests/test_views.py
@@ -1,4 +1,5 @@
 from django.test import Client, TestCase
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from model_mommy import mommy
 
@@ -26,3 +27,29 @@ class CreateCartItemTestCase(TestCase):
         response = self.client.get(self.url)
         cart_item = CartItem.objects.get()
         self.assertEqual(cart_item.quantity, 2)
+
+
+class CheckoutViewTestCase(TestCase):
+    def setUp(self):
+        self.user = mommy.prepare(settings.AUTH_USER_MODEL)
+        self.user.set_password('123')
+        self.user.save()
+        self.cart_item = mommy.make(CartItem)
+        self.client = Client()
+        self.checkout_url = reverse('checkout:checkout')
+    
+    def tearDown(self):
+        pass
+    
+    def test_checkout_view(self):
+        response = self.client.get(self.checkout_url)
+        redirect_url = '{}?next={}'.format(
+            reverse(settings.LOGIN_URL), self.checkout_url
+        ) 
+        self.assertRedirects(response, redirect_url)
+        self.client.login(username=self.user.username, password='123')
+        self.cart_item.cart_key = self.client.session.session_key
+        self.cart_item.save()
+        response = self.client.get(self.checkout_url)
+        self.assertTemplateUsed(response, 'checkout/checkout.html')
+
