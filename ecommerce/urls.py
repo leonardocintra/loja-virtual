e664e4e3665007e9fc11ca27cfdb6faf95c528a8
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-11 22:41:47 -0200

    exibindo imagens dos produtos

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,6 +1,8 @@
 """ URLS Ecommerce """
 
+from django.conf import settings
 from django.conf.urls import url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
 
@@ -19,3 +21,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
