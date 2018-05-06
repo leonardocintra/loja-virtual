4535e10413ae44aa6202c42eb87e1b6daf1ebb6c
Glauber <glauber.tecinfo@gmail.com>
2018-05-06 15:28:17 -0300

    Fixed wrong NAME in AUTH_PASSWORD_VALIDATORS

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthSTATIC_ROOTValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
